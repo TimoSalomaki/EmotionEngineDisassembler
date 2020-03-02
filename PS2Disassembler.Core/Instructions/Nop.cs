22e5e1023bb502e5764868d60c0337edad14607e
TimoSalomäki <tsalomaki@gmail.com>
2020-03-02 17:17:43 +0100

    Syntax highlighting for the assembly

--- PS2Disassembler.Core/Instructions/Nop.cs
+++ PS2Disassembler.Core/Instructions/Nop.cs
@@ -9,7 +9,7 @@
 
         public override string ToString()
         {
-            return "nop";
+            return "NOP";
         }
     }
 }
--- PS2Disassembler/MainWindow.xaml.cs
+++ PS2Disassembler/MainWindow.xaml.cs
@@ -1,5 +1,9 @@
-﻿using PS2Disassembler.ViewModel;
+﻿using ICSharpCode.AvalonEdit.Highlighting;
+using ICSharpCode.AvalonEdit.Highlighting.Xshd;
+using PS2Disassembler.ViewModel;
+using System;
 using System.Windows;
+using System.Xml;
 
 namespace PS2Disassembler
 {
@@ -14,8 +18,23 @@ namespace PS2Disassembler
         {
             InitializeComponent();
 
+            var assembly = this.GetType().Assembly;
+
+            using (var stream = assembly.GetManifestResourceStream(assembly.GetName().Name + ".MIPSSyntax.xshd"))
+            {
+                using (var reader = new XmlTextReader(stream))
+                {
+                    AssemblyEditor.SyntaxHighlighting = HighlightingLoader.Load(reader, HighlightingManager.Instance);
+                }
+            }
+
             _vm = new MainViewVM();
             this.DataContext = _vm;
+        }
+
+        protected override void OnInitialized(EventArgs e)
+        {
+            base.OnInitialized(e);
         }
     }
 }
